08733f65e5ae0f8d8fc8d1df5b19beefe0f260e7
Matheus <matheusrato_97@hotmail.com>
2020-03-18 11:44:26 -0300

    Commit

---
+++ app/src/main/java/com/example/cadastro/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.cadastro;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnCadastro = (Button) findViewById(R.id.btnCadastro);
+
+        btnCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cad = new Intent(this, )
+            }
+        });
+
+    }
+}
